48bbdaa9cab47fe0fe3ec4e5eb0dc72b8e4a12f3
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-11-24 08:59:23 +0530

    Refactor palindrome check in C program for clarity and consistency

--- C/CourseWork/palindrome.c
+++ C/CourseWork/palindrome.c
@@ -1,16 +1,13 @@
 #include<stdio.h>
 #include<string.h>
 
+
 int main(){
-    char c[] = "madam";
-    int len, i;
-    len = strlen(c);
-    for (i = 0; i < len/2; i++){
-        if (c[i] != c[len - i - 1]){
-            printf("Not a palindrome");
-            return 0;
-        }
+    char string[] = "malayalam";
+    for (int i = 0 ; i < strlen(string)/2 ; i++){
+        if (string[i] != string[strlen(string) - i - 1]) {printf("This is not a palindrome\n"); return 0;}
     }
-    printf("It is a palindrome");
+    printf("This is a palindrome");
+
     return 0;
 }
