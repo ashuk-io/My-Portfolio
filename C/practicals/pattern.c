794234543325e6bac934f30b3c6929fe5a91b51e
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-12-08 14:49:31 +0530

    Refactor diamond pattern program to simplify structure and improve readability

--- C/practicals/pattern.c
+++ C/practicals/pattern.c
@@ -1,38 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int n, i, j;
-    
-    // The user enters the size of the diamond (half the height)
-    printf("Enter the size (number of rows for the top half): ");
-    scanf("%d", &n);
-
-    // --- Part 1: Upper Pyramid (The Top Half) ---
-    for (i = 1; i <= n; i++) {
-        // Print leading SPACES
-        for (j = 1; j <= n - i; j++) {
+    for(int i = 0 ; i < 10 ; i++){
+        for ( int j = 0 ; j < 10 - i ; j++){
             printf(" ");
         }
-        // Print STARS
-        for (j = 1; j <= 2*i - 1; j++) {
-            printf("*");
+        for ( int j = 0 ; j < i ; j++){
+            printf("* ");
         }
         printf("\n");
-    }
 
-    // --- Part 2: Lower Inverted Pyramid (The Bottom Half) ---
-    // Start from the second-to-last row of the top part (n-1) and go down to 1
-    for (i = n - 1; i >= 1; i--) { 
-        // Print leading SPACES
-        for (j = 1; j <= n - i; j++) {
+    }
+    for(int i = 10 ; i > 0 ; i--){
+        for ( int j = i ; j > i-1  ; j--){
             printf(" ");
         }
-        // Print STARS
-        for (j = 1; j <= 2 * i - 1; j++) {
-            printf("*");
+        for ( int j = i ; j > i ; j--){
+            printf("* ");
         }
         printf("\n");
-    }
 
+    }
     return 0;
+    
 }
