e571fe7ed54d57b3f94a70b0406fa7244692c247
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-12-15 13:32:06 +0530

    TypeCasting

---
+++ Python/Python Course/04_typecasting/01_typecasting.py
@@ -0,0 +1,12 @@
+a = 19
+print(a , type(a))
+
+b = "19"
+print(b, type(b))
+
+c = int(b)
+print(c , type(c))
+
+d=207
+e = str(d)
+print(e , type(e))
---
+++ Python/Python Course/04_typecasting/02_example.py
@@ -0,0 +1,13 @@
+
+num_str = "10"
+num_int = int(num_str)
+print(num_int, type(num_str)) 
+
+num = 25
+num_str = str(num)
+print(num_str, type(num_str))  
+
+
+pi = 3.14
+pi_int = int(pi)
+print(pi_int, type(pi_int))
