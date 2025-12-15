6191a9f48a8cdfedec40ef5b3cfba1e1519d64c9
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-12-15 13:14:49 +0530

    Basics and Data Types

---
+++ Python/Python Course/03_variables/01_basics.py
@@ -0,0 +1,9 @@
+age = 34  #integer
+name = "Ashu"  #string
+cgpa = 9.2  #float
+
+#variable cant start with a numebr 
+#variable cant contain special characters
+
+__age__ = 32
+__nice_07 = "Variable"
---
+++ Python/Python Course/03_variables/02_datatypes.py
@@ -0,0 +1,15 @@
+age = 19
+print(age)
+print(type(age))
+
+name = "Ashu"
+print(name)
+print(type(name))
+
+cgpa = 9.3
+print(cgpa)
+print(type(cgpa))
+
+is_completed = True #can also be False
+print(is_completed)
+print(type(is_completed))
