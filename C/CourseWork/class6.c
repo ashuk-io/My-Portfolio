c54f1e0be97397939972eb5a4ca9916b71249240
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-12-15 11:36:21 +0530

    Max Number  in a Array fix

--- C/CourseWork/class6.c
+++ C/CourseWork/class6.c
@@ -2,11 +2,11 @@
 int main() {
     int data[5] = {45,72,29,90,61};
     int max = data[0];
-    for (int i = 1; i<5, i++;){
-        if(data[1] > max) {
-            max = data[1];
-            
+    for ( int i = 0 ; i <= 5 ; i++){
+        if(data[i] > max ){
+            max = data[i];
         }
+        else continue;
     }
     printf("Maximum = %d", max);
         return 0;
