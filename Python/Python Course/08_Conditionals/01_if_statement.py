c371fea1a3e69be4ac99c1004b52cebbc667ed46
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-12-15 21:09:15 +0530

    Conditional Statements

---
+++ Python/Python Course/08_Conditionals/01_if_statement.py
@@ -0,0 +1,4 @@
+a = 19
+if ( a >18):
+    print("You can drive")
+print("End of program")
---
+++ Python/Python Course/08_Conditionals/02_if_elif_else.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age : "))
+
+if (age >= 18):
+    print("You can drive")
+elif(age == 18):
+    print("Lets schedule an interview")
+else:
+    print("You cant drive yet")
+print("End of program")
