fecda8a109a760b44291e1bd253d4142bb93a51a
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-12-18 22:57:15 +0530

    Tuples, unpacking and methods

---
+++ Python/Python Course/13_tuples.py/01_tuples.py
@@ -0,0 +1,7 @@
+a = (2,11,23,17,)
+print(a)
+print(a[2])
+# a[3] = 9 # Error -> Tuple does not support modificaiton after creation
+
+b = (5)
+print(b, ) # comma needed for single element tuple
---
+++ Python/Python Course/13_tuples.py/03_tuple_methods.py
@@ -0,0 +1,3 @@
+t = (3,12,45,87,55)
+print(t.count(12))
+print(t.index(55))
