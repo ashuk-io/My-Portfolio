e1de901f795ccca0e9d2313059ccf850183cecd1
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2025-12-18 20:22:21 +0530

    List, Methods and comprehension

---
+++ Python/Python Course/12_lists/01_lists.py
@@ -0,0 +1,7 @@
+marks = [56,87,51,76,27,94]
+mixed = [43,'Hello', False, 4.2]
+
+print(marks[2:4])
+print(mixed)
+print(mixed[1:])
+# print(mixed[4]) # Error -> Out of range/bound
---
+++ Python/Python Course/12_lists/02_list_methods.py
@@ -0,0 +1,17 @@
+marks = [5,21,5,7]
+extra_marks = [53,23,32]
+print(marks)
+marks.append(63) # This will change the original list
+
+print(marks)
+marks.pop()
+print(marks)
+marks.extend(extra_marks)
+print(marks)
+print(marks.count(5))
+marks.insert(7,99)
+print(marks)
+marks.remove(23)
+print(marks)
+marks.sort()
+print(marks)
