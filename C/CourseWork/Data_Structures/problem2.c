96b7f9595504919a9197d82afffaa3463757e005
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2026-01-07 11:12:01 +0530

    DS Class1

---
+++ C/CourseWork/Data_Structures/problem2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int a[10];
+    int sum = 0;
+    for (int i = 0 ; i <10 ; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+    printf("\nSum: %d\n", sum);
+}
---
+++ C/CourseWork/Data_Structures/problem3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int a[10];
+    for (int i = 0 ; i <10 ; i++) scanf("%d", &a[i]);
+    for (int i = 0 ; i <10 ; i++) {
+        if ( a[i] % 2 == 0 ) printf("%d ", a[i]);
+    }
+    return 0;
+}
