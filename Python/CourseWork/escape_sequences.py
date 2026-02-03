881046309c9f32057ba3b39efc4360fe4ad8db10
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2026-02-03 21:22:34 +0530

    escape sequence

---
+++ Python/CourseWork/escape_sequences.py
@@ -0,0 +1,20 @@
+
+num = 7
+amt = 213.45
+code = 'A'
+pi = 3.1415
+population_of_india = 1400000
+
+msg = 'Hi'
+
+print('NUM = '+ str(num))
+print('\nAMT = '+ str(amt))
+print('\nCODE = '+ str(code))
+print('\nPOPULATION OF INDIA = '+ str(population_of_india))
+print('\nMESSAGE = '+ str(msg))
+
+
+
+
+
+
