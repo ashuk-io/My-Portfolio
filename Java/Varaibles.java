1767a59081aa2a8905426a66ea66cae2fb48966b
Ashutosh Kumar <ashutoshjpis.ncs@gmail.com>
2026-05-24 00:09:23 +0530

    Add Varaibles class with basic multiplication and input functionality

---
+++ Java/Varaibles.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Varaibles {
+    public static void main(String args[]){
+        int a = 20;
+        int b = 10;
+        int mul = a * b;
+
+        System.out.println(mul);
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+    }
+    
+}
