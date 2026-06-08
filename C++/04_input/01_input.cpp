c9cf4e68169b24adb1fe7e6e20d39f082d460683
ashuk-io <ashutoshjpis.ncs@gmail.com>
2026-06-08 20:43:33 +0530

    input

---
+++ C++/04_input/01_input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace  std;
+int main() {
+    int num1 = 10;
+    int num2;
+    cout << "Enter the 2nd number :\n";
+    cin >> num2;
+    cout << "Sum = " << num1+num2 << endl;
+    return 0;
+}
